c8f284e930ee7b531937a370e8a6947e87f42e2c
PibeDx <jcuentast@gmail.com>
2016-12-30 17:45:40 -0500

    feat implement example

--- app/src/main/java/com/josecuentas/android_spannable/MainActivity.java
+++ app/src/main/java/com/josecuentas/android_spannable/MainActivity.java
@@ -1,8 +1,12 @@
 package com.josecuentas.android_spannable;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Spanned;
 import android.widget.TextView;
+
+import com.josecuentas.android_spannable.lib.SpanDecorationBuild;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +33,28 @@ public class MainActivity extends AppCompatActivity {
         lastname = "Cuentas Turpo";
         message = "";
         email = "jcuentast@gmail.com";
+        String normal = " ==> normal";
+
+
+        Spanned ssbUsername = SpanDecorationBuild.builder(this).spanTypeface(username, Typeface.BOLD).append(normal).builder();
+        Spanned ssbName = SpanDecorationBuild.builder(this).spanColor(name, R.color.colorAccent).append(normal).builder();
+        Spanned ssbLastname= SpanDecorationBuild.builder(this).spanColorAndTypeface(lastname, R.color.colorPrimaryDark, Typeface.BOLD).append(normal).builder();
+        Spanned ssbMessage = SpanDecorationBuild
+                .builder(this)
+                .append("Message: \n========\n")
+                .spanColorAndTypeface(username + "\n", R.color.colorPrimary,Typeface.BOLD)
+                .spanColor(name + "\n", R.color.colorAccent)
+                .spanColorAndTypeface(lastname, R.color.colorPrimaryDark, Typeface.BOLD)
+                .append("\n========")
+                .builder();
+        Spanned ssbEmail = SpanDecorationBuild.builder(this).spanTypeface(email, Typeface.BOLD).append(normal).builder();
+
+        mTviUsername.setText(ssbUsername, TextView.BufferType.SPANNABLE);
+        mTviName.setText(ssbName);
+        mTviLastName.setText(ssbLastname);
+        mTviMessage.setText(ssbMessage);
+        mTviEmail.setText(ssbEmail);
+
 
     }
 
